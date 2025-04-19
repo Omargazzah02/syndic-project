cb5bb1d3cd008e38835c361f6c11c675b90b090f
Youssef <youssefsghier7@gmail.com>
2025-04-19 20:25:00 +0100

    page manager linking interventions

--- interventions/urls.py
+++ interventions/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import InterventionListByResidenceAPIView, CreateInterventionAPIView
+from .views import InterventionListByResidenceAPIView, CreateInterventionAPIView ,UpdateInterventionStatus, InterventionDetail  
 
 urlpatterns = [
     path('interventions/<int:residence_id>/', InterventionListByResidenceAPIView.as_view(), name='intervention-list-by-residence'),
     path('interventions/create/', CreateInterventionAPIView.as_view(), name='intervention-create'),
+    path('interventions/update-status/<int:intervention_id>/', UpdateInterventionStatus.as_view(), name='update-intervention-status'),
+    path('interventions/delete/<int:pk>/', InterventionDetail.as_view(), name='intervention-detail'),  # Uncomment if needed
+
+
 ]
--- interventions/views.py
+++ interventions/views.py
@@ -30,3 +30,36 @@ class CreateInterventionAPIView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
+
+
+
+class UpdateInterventionStatus(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def patch(self, request, intervention_id):
+        try:
+            intervention = Intervention.objects.get(id=intervention_id)
+            new_status = request.data.get('status')
+            if new_status:
+                intervention.status = new_status
+                intervention.save()
+                return Response({"message": "Statut mis à jour"}, status=status.HTTP_200_OK)
+            else:
+                return Response({"error": "Statut manquant"}, status=status.HTTP_400_BAD_REQUEST)
+        except Intervention.DoesNotExist:
+            return Response({"error": "Intervention non trouvée"}, status=status.HTTP_404_NOT_FOUND)
+
+
+
+# views.py (extrait pour DELETE)
+class InterventionDetail(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def delete(self, request, pk):
+        try:
+            intervention = Intervention.objects.get(pk=pk)
+            intervention.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Intervention.DoesNotExist:
+            return Response({'error': 'Intervention non trouvée'}, status=404)
