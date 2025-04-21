072c0eb97eeb7f6342ba1d4aee5686ac5d73de5b
Omargazzah02 <gazzahomar2001@gmail.com>
2025-04-21 21:58:29 +0200

    charges_factures

--- documents_app/admin.py
+++ documents_app/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Document
+from .models import Document, Invoice
 # Register your models here.
 
 admin.site.register(Document)
+
+@admin.register(Invoice)
+class InvoiceAdmin(admin.ModelAdmin):
+    exclude = ('residence','category', )  # ne pas l'afficher dans l'admin
--- documents_app/models.py
+++ documents_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from residences_app.models import Residence
 from django.core.validators import FileExtensionValidator
+from charges_app.models import Charge
 
 
 # Create your models here.
@@ -11,7 +12,8 @@ class Document (models.Model) :
         ('Documents techniques' , 'Documents techniques') , 
         ('Documents relatifs à la gestion des travaux' , 'Documents relatifs à la gestion des travaux'),
         ('Documents relatifs aux assurances' , 'Documents relatifs aux assurances') ,
-        ('Documents juridiques' , 'Documents juridiques')
+        ('Documents juridiques' , 'Documents juridiques'),
+        ('Factures' , 'Factures'),
     ]
     residence = models.ForeignKey(Residence, on_delete=models.CASCADE, related_name='documents')
     category = models.CharField(max_length=100 , choices=CATEGORY_CHOICES)
@@ -22,3 +24,22 @@ class Document (models.Model) :
 
     def __str__(self):
         return self.title
+
+
+
+
+class Invoice (Document) : 
+
+        charge = models.OneToOneField(
+        Charge,
+        on_delete=models.CASCADE,
+        related_name='invoice',
+
+    )
+        def save(self , *args, **kwargs):
+             if self.charge:
+                  self.residence = self.charge.residence
+                  self.category = "Factures"
+             super().save(*args, **kwargs)
+
+    
